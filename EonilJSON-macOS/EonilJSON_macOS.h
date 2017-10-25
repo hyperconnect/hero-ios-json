66b49a87260afb5ce6b3330905052cee0ddd1378
Hoon H <hh@hpcnt.com>
2017-10-25 10:06:54 +0900

    Make macOS static library target.

---
+++ EonilJSON-macOS/EonilJSON_macOS.h
@@ -0,0 +1,13 @@
+//
+//  EonilJSON_macOS.h
+//  EonilJSON-macOS
+//
+//  Created by Hoon Hwangbo on 10/25/17.
+//  Copyright © 2017 Eonil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EonilJSON_macOS : NSObject
+
+@end
---
+++ EonilJSON-macOS/EonilJSON_macOS.m
@@ -0,0 +1,13 @@
+//
+//  EonilJSON_macOS.m
+//  EonilJSON-macOS
+//
+//  Created by Hoon Hwangbo on 10/25/17.
+//  Copyright © 2017 Eonil. All rights reserved.
+//
+
+#import "EonilJSON_macOS.h"
+
+@implementation EonilJSON_macOS
+
+@end
